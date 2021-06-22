b0e91043e1584d7cea61739903b27ff1ce66bc4f
Aaron Krumsieg <a.krumsieg@gmail.com>
2021-06-22 13:17:02 -0500

    Add CustomerController with Index method -- Add Index view -- Add Customers DbSet in IdentityModels

---
+++ GeneralStore.MVC/Controllers/CustomerController.cs
@@ -0,0 +1,22 @@
+﻿using GeneralStore.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GeneralStore.MVC.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private ApplicationDbContext _db = new ApplicationDbContext();
+
+        // GET: Customer
+        public ActionResult Index()
+        {
+            List<Customer> customerList = _db.Customers.ToList();
+            List<Customer> orderedList = customerList.OrderBy(c => c.LastName).ToList();
+            return View(orderedList);
+        }
+    }
+}
