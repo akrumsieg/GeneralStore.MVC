594550156e15fbbbe4d7b675b5cc31109c8c88ae
Aaron Krumsieg <a.krumsieg@gmail.com>
2021-06-22 13:28:25 -0500

    Add Customer Delete methods and view

--- GeneralStore.MVC/Controllers/CustomerController.cs
+++ GeneralStore.MVC/Controllers/CustomerController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -36,6 +37,28 @@ namespace GeneralStore.MVC.Controllers
                 return RedirectToAction("Index");
             }
             return View(customer);
+        }
+
+        // GET: Delete
+        //Customer/Delete/{id}
+        public ActionResult Delete(int? id)
+        {
+            if (id == null) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            Customer customer = _db.Customers.Find(id);
+            if (customer == null) return HttpNotFound();
+            return View(customer);
+        }
+
+        // POST: Delete
+        //Customer/Delete/{id}
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            Customer customer = _db.Customers.Find(id);
+            _db.Customers.Remove(customer);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
